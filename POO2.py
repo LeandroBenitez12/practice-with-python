67a82b76640cfb370809ccd95a3236563e7b49d9
Leandro Benitez <leansvae9@gmail.com>
2022-09-21 00:22:43 -0300

    continuando curso e.90

--- POO2.py
+++ POO2.py
@@ -2,4 +2,15 @@ from msilib.schema import Class
 
 
 Class Automovil:
-    pass
+    def __init__(self)
+    self.marca = 'Volkswagen'
+    self.modelo ='Golf'
+    self.precio = 3000000
+    self.patente = 'AD2S56A'
+    self.puertas = 4
+    self.color = 'Black'
+    
+Camion = Automovil()
+print(Camion.marca)
+print(Camion.modelo)
+print(Camion.precio)
