cc79fa92b5d256e7e2d52aa51f11484023470ea7
Leandro Benitez <leansvae9@gmail.com>
2022-09-21 15:10:49 -0300

    clase 96 poo

--- POO2.py
+++ POO2.py
@@ -1,16 +1,19 @@
 class Car:
-    def __init__(self, tipo, marca, mod, precio, patente, puertas, color):
+    def __init__(self, tipo, marca, mod, precio, color):
         self.tipo = tipo
         self.marca =  marca
         self.modelo = mod
         self.precio = precio
-        self.patente =  patente
-        self.puertas = puertas
         self.color =  color
     
-Car_c1 = Car('car Segment A','reno','clio', 1500000, 'ads0bc', 3 , 'gray')
-Truck = Car('Truck','Mercedes Benz','Atego', 1500000, 'ao4lq2', 3 , 'brown')
+    def mostrar_detalle(self):
+        print(f'Car: {self.marca} {self.modelo} {self.precio}, {self.color}')
 
-print(Truck.marca)
-print(Car_c1.modelo)
-print(Truck.precio)
+Car_c1 = Car('car Segment A','reno','clio', 1500000, 'gray')
+Truck = Car('Truck','Mercedes Benz','Atego', 12345554300, 'brown')
+
+Car_c1.mostrar_detalle()
+
+Truck.km = 40032
+
+print(Truck.km)
