de186c80d2a08bb1210ff1be14dab2e394950508
Leandro Benitez <leansvae9@gmail.com>
2022-09-21 00:46:14 -0300

    calase 91 python

--- POO2.py
+++ POO2.py
@@ -1,5 +1,6 @@
-class Automovil:
-    def __init__(self, marca, mod, precio, patente, puertas, color):
+class Car:
+    def __init__(self, tipo, marca, mod, precio, patente, puertas, color):
+        self.tipo = tipo
         self.marca =  marca
         self.modelo = mod
         self.precio = precio
@@ -7,7 +8,9 @@ class Automovil:
         self.puertas = puertas
         self.color =  color
     
-Camion = Automovil('reno','clio', 1500000, 'ads0bc', 3 , 'gris')
-print(Camion.marca)
-print(Camion.modelo)
-print(Camion.precio)
+Car_c1 = Car('car Segment A','reno','clio', 1500000, 'ads0bc', 3 , 'gray')
+Truck = Car('Truck','Mercedes Benz','Atego', 1500000, 'ao4lq2', 3 , 'brown')
+
+print(Truck.marca)
+print(Car_c1.modelo)
+print(Truck.precio)
