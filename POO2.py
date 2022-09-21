c3ea2f077fdcc056b74a97799de9f5916bd16185
Leandro Benitez <leansvae9@gmail.com>
2022-09-21 00:30:34 -0300

    clases de python con parametros para atributos

--- POO2.py
+++ POO2.py
@@ -1,16 +1,13 @@
-from msilib.schema import Class
-
-
-Class Automovil:
-    def __init__(self)
-    self.marca = 'Volkswagen'
-    self.modelo ='Golf'
-    self.precio = 3000000
-    self.patente = 'AD2S56A'
-    self.puertas = 4
-    self.color = 'Black'
+class Automovil:
+    def __init__(self, marca, mod, precio, patente, puertas, color):
+        self.marca =  marca
+        self.modelo = mod
+        self.precio = precio
+        self.patente =  patente
+        self.puertas = puertas
+        self.color =  color
     
-Camion = Automovil()
+Camion = Automovil('reno','clio', 1500000, 'ads0bc', 3 , 'gris')
 print(Camion.marca)
 print(Camion.modelo)
 print(Camion.precio)
