c9de047144cfe1bb3f2edd00abf8dbc70b5d98e4
Leandro Benitez <leansvae9@gmail.com>
2022-09-24 22:36:47 -0300

    Encapsulamos todos los atributos : )

--- POO2.py
+++ POO2.py
@@ -8,7 +8,7 @@ class Car:
 
     @property #decorador , modifica el metodo para solo acceder al atributo atravez del metodo, tipo te hace ahorrar los '()'
     def marca(self):# metodo get
-        print('llamando metodo get marca')
+        #print('llamando metodo get marca')
         return self._marca
 
     #@marca.setter #@(atribute_name.setter)
@@ -16,14 +16,32 @@ class Car:
     #    print('llamando metodo set marca')
     #    self._marca = marca
 
+    @property #decorador , modifica el metodo para solo acceder al atributo atravez del metodo, tipo te hace ahorrar los '()'
+    def modelo(self):# metodo get
+        #print('llamando metodo get modelo')
+        return self._modelo
+
+    @property #decorador , modifica el metodo para solo acceder al atributo atravez del metodo, tipo te hace ahorrar los '()'
+    def precio(self):# metodo get
+        #print('llamando metodo get precio')
+        return self._precio
+
+    @property #decorador , modifica el metodo para solo acceder al atributo atravez del metodo, tipo te hace ahorrar los '()'
+    def color(self):# metodo get
+        #print('llamando metodo get color')
+        return self._color
+
     def mostrar_detalle(self):
         print(f'Car: {self._marca} {self._modelo} {self._precio}, {self._color}')
 
 Car_c1 = Car('car Segment A','reno','clio', 1500000, 'gray')
 Truck = Car('Truck','Mercedes Benz','Atego', 12345554300, 'brown')
 
-Car_c1.marca = 'Mercedes'
-print(Car_c1.marca)#recuperamos private get 
+#Car_c1.marca = 'Mercedes'
+#Car_c1.modelo = 'm1b'
+#Car_c1.precio = 12121314
+#Car_c1.color = 'yelou'
+print(Car_c1.precio)#recuperamos private get 
 
 
 #Car_c1.mostrar_detalle()
