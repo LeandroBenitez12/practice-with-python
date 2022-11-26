2bccd430c02f916c2a8675f3e89b9b7a0f56a04b
Leandro Benitez <leansvae9@gmail.com>
2022-11-26 01:00:52 -0300

    ORDENE, ARREGLE PROGRAMAS

--- leccion 04 herencia multiple/Test_Figura_geometrica.py
+++ leccion 04 herencia multiple/Test_Figura_geometrica.py
@@ -1,8 +1,10 @@
 from Cuadrado import Cuadrado
 
-SquareGreen = Cuadrado( 4, 'Green')
+numero = int(input(" ingresa LA ALTURA Y ANCHO DE LOS LADOS DL PUTO CUADRAD0 el n la reputa que te pario:  "))
+color = str(input(" el color del CUADRAD0 (IMBECIL)ahora ingresa corneta:  "))
+SquareGreen = Cuadrado( numero, color)
 
-print(SquareGreen.ancho)
-print(SquareGreen.alto)
-print(SquareGreen.color)
-print(f'calculo del area del square: {SquareGreen.calcularArea()} ')
+print((f' el ancho del square :  {SquareGreen.ancho}' ))
+print((f' el alto del square :  {SquareGreen.alto}' ))
+print((f' el color del square :  {SquareGreen.color}' ))
+print(f'the calculo del area del square: {SquareGreen.calcularArea()} ')
