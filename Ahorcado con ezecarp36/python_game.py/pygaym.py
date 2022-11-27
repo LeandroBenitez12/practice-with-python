df61b10194edcb13c5578e2fb4ac0415c2326555
Leandro Benitez <leansvae9@gmail.com>
2022-11-26 22:25:44 -0300

    me faltan 2 videos por ahora va bien y bonito

---
+++ Ahorcado con ezecarp36/python_game.py/pygaym.py
@@ -0,0 +1,166 @@
+import turtle 
+import time
+import random
+
+posponer= 0.12
+recorrido = 20
+minimoArea= 290
+
+
+ventana =  turtle.Screen()
+ventana.title('Game of python')
+ventana.bgcolor('black')
+ventana.setup( width = 600, height= 600)
+ventana.tracer(0)
+
+cabeza = turtle.Turtle()
+cabeza.speed(0)
+cabeza.shape('square')
+cabeza.color('white')
+cabeza.penup()
+cabeza.goto(0,0)
+cabeza.direction = 'stop'
+
+cuerpos = []
+
+'''
+cabeza2 = turtle.Turtle()
+cabeza2.speed(0)
+cabeza2.shape('square')
+cabeza2.color('red')
+cabeza2.penup()
+cabeza2.goto(-100,0)
+cabeza2.direction = 'stop'
+'''
+comida = turtle.Turtle()
+comida.speed(0)
+comida.shape('circle')
+comida.color('yellow')
+comida.penup()
+comida.goto(0,100)
+
+
+def forward1():
+    cabeza.direction = 'forward'
+
+def backward1():
+    cabeza.direction = 'backward'
+
+def left1():
+    cabeza.direction = 'left'
+
+def right1():
+    cabeza.direction = 'right'
+'''
+def forward2():
+    cabeza2.direction = 'forward'
+
+def backward2():
+    cabeza2.direction = 'backward'
+
+def left2():
+    cabeza2.direction = 'left'
+
+def right2():
+    cabeza2.direction = 'right'
+'''
+def mov():
+    if cabeza.direction == 'forward':
+        y = cabeza.ycor()
+        cabeza.sety(y + recorrido)
+
+    if cabeza.direction == 'backward':
+        y = cabeza.ycor()
+        cabeza.sety(y - recorrido)
+
+    if cabeza.direction == 'right':
+        x = cabeza.xcor()
+        cabeza.setx(x + recorrido)
+
+    if cabeza.direction == 'left':
+        x = cabeza.xcor()
+        cabeza.setx(x - recorrido)
+
+    if cabeza.ycor() > minimoArea or cabeza.ycor() < -minimoArea:
+        cabeza.direction = 'stop'
+    
+    if cabeza.xcor() > minimoArea or cabeza.xcor() < -minimoArea:
+        cabeza.direction = 'stop'
+
+'''def mov2():
+    if cabeza2.direction == 'forward':
+        y = cabeza2.ycor()
+        cabeza2.sety(y + recorrido)
+
+    if cabeza2.direction == 'backward':
+        y = cabeza2.ycor()
+        cabeza2.sety(y - recorrido)
+
+    if cabeza2.direction == 'right':
+        x = cabeza2.xcor()
+        cabeza2.setx(x + recorrido)
+
+    if cabeza2.direction == 'left':
+        x = cabeza2.xcor()
+        cabeza2.setx(x - recorrido)
+
+    if cabeza2.ycor() > minimoArea or cabeza2.ycor() < -minimoArea:
+        cabeza2.direction = 'stop'
+    
+    if cabeza2.xcor() > minimoArea or cabeza2.xcor() < -minimoArea:
+        cabeza2.direction = 'stop'
+'''
+#teclado
+ventana.listen()
+ventana.onkeypress(forward1, 'Up')
+ventana.onkeypress(backward1, 'Down')
+ventana.onkeypress(left1, 'Left')
+ventana.onkeypress(right1, 'Right')
+
+'''
+ventana.onkeypress(forward2, 'w')
+ventana.onkeypress(backward2, 's')
+ventana.onkeypress(left2, 'a')
+ventana.onkeypress(right2, 'd')
+'''
+while True:
+    ventana.update()
+    #choques
+    if cabeza.xcor() > 280 or cabeza.ycor() < -280 or cabeza.ycor() > 280 or cabeza.xcor() < -280:
+        time.sleep(1)
+        cabeza.goto(0,0)
+        cabeza.direction = 'stop'
+
+        for cuerpo in cuerpos:
+            cuerpo.goto(1000,1000)
+
+        cuerpos.clear()
+    
+    if cabeza.distance(comida) < 20:
+        x = random.randint(-280, 280)
+        y = random.randint(-280, 280)
+        comida.goto(x,y)
+
+        
+
+        nuevoCuerpo = turtle.Turtle()
+        nuevoCuerpo.speed(0)
+        nuevoCuerpo.shape('square')
+        nuevoCuerpo.color('grey')
+        nuevoCuerpo.penup()
+        cuerpos.append(nuevoCuerpo)
+ 
+    #mover el corp the python
+    totalCuerpos = len(cuerpos)
+    for index in range(totalCuerpos -1, 0, - 1):
+        x = cuerpos[index -1].xcor()
+        y = cuerpos[index -1].ycor()
+        cuerpos[index].goto(x,y)
+
+    if totalCuerpos > 0:
+        x = cabeza.xcor()
+        y = cabeza.ycor()
+        cuerpos[0].goto(x,y)
+    mov()
+    #mov2()
+    time.sleep(posponer)
