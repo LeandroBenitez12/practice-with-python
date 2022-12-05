5dd55ecbed2f141439704dab27768e0577ebef2f
Leandro Benitez <leansvae9@gmail.com>
2022-12-05 00:21:54 -0300

    CALSE 124 TERMINADA Y ENTENDIDA REPASAR NOTACION SIMPLIFICADA

--- Herencia multiple/Laboratorio.FigGeometrica/FiguraGeometrica.py
+++ Herencia multiple/Laboratorio.FigGeometrica/FiguraGeometrica.py
@@ -1,19 +1,19 @@
 class FiguraGeometrica:
     def __init__(self, width , height):
         #validate if the variable width or height is less than 50
-        if 0 <= width < 50:
+        if self._validate_value(width):
             self._width = width
         else:
             self.width = 0
+            print(f'value error width : {width}')
         
-        if 0 <= height < 50:
+        if self._validate_value(height):
             self._height = height
         else:
             self.height = 0
+            print(f'value error height : {height}')
 
-    def __str__(self):
-        return f'the geometric figure have a height of : {self._height}, and a width of :  {self._width}'
-    
+
     #set = modificar| get = obtener
     @property #decorador , modifica el metodo para solo acceder al atributo atravez del metodo, tipo te hace ahorrar los '()'
     def width(self):
@@ -21,7 +21,10 @@ class FiguraGeometrica:
     
     @width.setter #metodo de tipo set asociado a ese atributo
     def width(self, width):
-        self._width = width
+        if self._validate_value(width):#propagamos errores a los setter
+            self._width = width
+        else:
+            print(f'value error width : {width}')
     
     @property
     def height(self):
@@ -29,4 +32,14 @@ class FiguraGeometrica:
     
     @height.setter #metodo de tipo set asociado a ese atributo
     def height(self, height):
-        self._height = height
+        if self._validate_value(height):#propagamos errores a los setter
+            self._height = height
+        else:
+            print(f'value error height : {height}')
+
+
+    def __str__(self):
+        return f'the geometric figure have a height of : {self._height}, and a width of :  {self._width}'
+    
+    def _validate_value(self, value):#initialize width a '-' to indicate method private
+        return True if 0 < value < 10 else False # Retornamos validacion sintaxis simplificada if else
--- Herencia multiple/Laboratorio.FigGeometrica/testLaboratorio.py
+++ Herencia multiple/Laboratorio.FigGeometrica/testLaboratorio.py
@@ -1,12 +1,18 @@
 from Cuadrado import Cuadrado
 from Rectangulo import Rectangulo
 
+LADOS = 4
+
 print('Creation Object Square'.center(50, '-'))#rellenamos con guiones medios
-Square = Cuadrado( lado=9, color= 'blue')#autodocumentamos codigo
+Square = Cuadrado( lado=8, color= 'blue')#autodocumentamos codigo
+Square.width = LADOS
+Square.height = LADOS
 print(f'The calculate area is {Square.calcularArea()}')
 print(Square)
 
+print('')
 print('Creation Object Rectangle'.center(50, '-'))#rellenamos con guiones medios
-Rectangle = Rectangulo(height= 51, width= 25,  color= 'Red')#autodocumentamos codigo
+Rectangle = Rectangulo(height= 4, width= 2,  color= 'Red')#autodocumentamos codigo
+Rectangle.height = 14
 print(f'The calculate area is {Rectangle.calcularArea()}')
 print(Rectangle)
