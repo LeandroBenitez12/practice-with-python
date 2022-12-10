325fa8a6e45013a80db62aad18b2743782112eb6
LeandroBenitez12 <Leansvae9@gmail.com>
2022-12-10 20:08:00 -0300

    142. if isinstance

--- Universidad Python/leccion 09 polimorfismo/test_polimorfismo.py
+++ Universidad Python/leccion 09 polimorfismo/test_polimorfismo.py
@@ -5,9 +5,13 @@ def print_detail(object):
     
     print(type(object))
     print(object.show_detail())
+    if isinstance(object, Manager):
+    # preguntamos si el objeto que introducimos es manager entonces que imprima el atributo department
+    #sino no imprime nada y no sale error si introducimos otro objeto, ej la clase  padre
+        print(object._department)
 
 Employee1 = Employee('David', 140000)
 Manager1 = Manager('lEANDRO', 240000, 'BOLU2')
 
-#print_detail(Employee1)
+print_detail(Employee1)
 print_detail(Manager1)
