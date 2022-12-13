4654b266a86539a2138470d19e97420fcd62cac8
Leandro Benitez <leansvae9@gmail.com>
2022-12-12 23:04:07 -0300

    Procesar clases de excepcion mas especificas

--- Universidad Python/Excepciones/Leccion01.py
+++ Universidad Python/Excepciones/Leccion01.py
@@ -4,12 +4,17 @@ b = 0
 
 try:
     result = a/b
-except ZeroDivisionError as e:#la clase ZeroDivisionError es una clase hija 
+except ZeroDivisionError as z:#la clase ZeroDivisionError es una clase hija 
     #, una sub clase digamos, si nosotros utilizamos Exception que es la clase padre,
     #ante cualquier error nosotros acutuamos protegiendo de la interrupcion de la app e informando el error
+    #lo recomendable es usar las sub clases, o clases hijas , y luego al final por las dudas poner la clase PADRE
+    print(f'ZeroDivisionError - Error processing {z}, {type(z)}')
 
-    print(f'Error processing {e}')
+except TypeError as t:
+    print(f'TypeError - Error processing: {t}, {type(t)}')
 
+except Exception as e:
+    print(f'Exception - Error processing: {e}, {type(e)}')
 
 print(f'The Result is: {result}')
 print('CONTINUAMOS...')
