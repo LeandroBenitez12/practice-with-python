7f7fa9d4fb3495596c2791aca320723c1d81cf26
Leandro Benitez <leansvae9@gmail.com>
2022-12-12 23:50:11 -0300

    Mas cositas de excepciones

--- Universidad Python/Excepciones/Leccion01.py
+++ Universidad Python/Excepciones/Leccion01.py
@@ -1,8 +1,9 @@
-result = None #No tiene value aun
-a = '10'
-b = 0
+result = None #No tiene value aun, TIENE QUE ESTAR ANTES DEL TRY PARA SER UTILIZADO LASOLUCION DE LA EXCEPCION COMO TAL
+# SI es declarada dentro del try luego va a salir error si entra en una excepcion como que la var no esta declarada
 
 try:
+    a = '10'
+    b = 0
     result = a/b
 except ZeroDivisionError as z:#la clase ZeroDivisionError es una clase hija 
     #, una sub clase digamos, si nosotros utilizamos Exception que es la clase padre,
