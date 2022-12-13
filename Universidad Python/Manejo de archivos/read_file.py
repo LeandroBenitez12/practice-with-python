e797ed175c71c92b4504d872f6e469103dc71127
Leandro Benitez <leansvae9@gmail.com>
2022-12-13 20:24:28 -0300

    158.Más formas de trabajar con Archivos en Python

--- Universidad Python/Manejo de archivos/read_file.py
+++ Universidad Python/Manejo de archivos/read_file.py
@@ -2,5 +2,24 @@ archivo = open('D:\\GIT\\Course Phyton\\Universidad Python\\Manejo de archivos\\
 #print(archivo.read())#comentar esta linea para leer alguno caracteres
 
 #leer algunos caracteres
-print(archivo.readline())
-print(archivo.readline())
+#print(archivo.readline())
+#print(archivo.readline())
+
+# iterar el archivo
+#for linea in archivo:
+#    print(linea)
+
+# leer lines
+#print(archivo.readlines())
+
+#aceder a una linea de la lista 
+#print(archivo.readlines()[2])
+
+#abrir new file 
+#anexamos a informacion
+archivo2 = open('D:\\GIT\\Course Phyton\\Universidad Python\\Manejo de archivos\\copia.txt', 'w', encoding= 'utf8')
+archivo2.write(archivo.read())
+
+archivo.close()
+archivo2.close()
+print('se termino the process de leer y copiar archivos')
