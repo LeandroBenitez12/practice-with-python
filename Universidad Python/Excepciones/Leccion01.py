033f4ca07deb82bd865115e1766ba44e9b275c82
Leandro Benitez <leansvae9@gmail.com>
2022-12-12 22:57:37 -0300

    Type error

--- Universidad Python/Excepciones/Leccion01.py
+++ Universidad Python/Excepciones/Leccion01.py
@@ -1,10 +1,13 @@
 result = None #No tiene value aun
-a = 10
+a = '10'
 b = 0
 
 try:
     result = a/b
-except Exception as e:
+except ZeroDivisionError as e:#la clase ZeroDivisionError es una clase hija 
+    #, una sub clase digamos, si nosotros utilizamos Exception que es la clase padre,
+    #ante cualquier error nosotros acutuamos protegiendo de la interrupcion de la app e informando el error
+
     print(f'Error processing {e}')
 
 
