c7bb388294336d7792fea0def68382555ea2a3bb
Leandro Benitez <leansvae9@gmail.com>
2022-12-15 23:07:04 -0300

    llegue hasta aqui

---
+++ Universidad Python/LABORATORIO CINE/CATALOGO_PELICULAS/test_catalogo_peliculas.py
@@ -0,0 +1,25 @@
+
+ValueMenu = None
+while ValueMenu != 4:
+    try:
+        print('''
+            ----------Menu----------
+            1.Agregar pelicula
+            2.Listar pelicula
+            3.Eliminar pelicula
+            4. Salir
+            '''
+            )
+        ValueMenu = int(input(f' Ingrese un valor(1-4): '))        
+        if ValueMenu == 1:
+
+        elif ValueMenu == 1:
+
+        elif ValueMenu == 1:
+
+    except Exception as e:
+        print ("Ocurrio un error {e}")
+        ValueMenu = None
+
+else:
+    print ("Salimos del programa...")
