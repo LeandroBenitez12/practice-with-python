4469cf1f6d8901f7baaa9853b18d75215b920da9
Leandro Benitez <leansvae9@gmail.com>
2022-12-17 12:49:26 -0300

    174 insert an regitre

--- Universidad Python/Base de Datos/Leccion 01/prueba_db.py
+++ Universidad Python/Base de Datos/Leccion 01/prueba_db.py
@@ -6,25 +6,16 @@ try:
     with conexion:  #para asegurar que se cierre cualquier recuerso asociado a conecion se cierre
         #cursor es un obejto que nos permite ejecutar sentencias SQL en postgres
         with conexion.cursor() as cursor:
-            #sentencia = 'SELECT * FROM persona WHERE id_persona = %s' #recuperar un registro 
-            sentencia = 'SELECT * FROM persona WHERE id_persona IN %s' #recuperar varios registros
-            
-            #id_persona = int(input('Ingrese el id_persona: ')) #para un registro
-            #llaves_primarias = ((1,2,3),)
-            entrada = input('Ingrese los id\'s a buscar(separados por comas):' )
-            llaves_primarias = (tuple(entrada.split(',')),)#EL SPLIT SACA LAS , y convertimos los numeros en tupla los ponemos en parentesis y hacemos otra tuplado de esa tupla 
-            
-            #cursor.execute(sentencia, '''(id_persona,)''') #la ',' es para que sea una tupla y no una simple variable
-            cursor.execute(sentencia, llaves_primarias)
-            
-            registros = cursor.fetchall()   #regresa todos los registros
-            #registro = cursor.fetchone()   #regresa un registro queda bien con lo de id_persona = %S ya que es nomas un registro
-            
-            print(registros)
-            #estamos conectados exitosamente a la base de datos
+            sentencia = 'INSERT INTO persona (name, surname, email) VALUES (%s, %s, %s)'
+            valores= ('Carlos', 'JEQUE', 'Jequec@live.com') #tupla de valores
+            cursor.execute(sentencia, valores)
+            #conexion.commit()  GUARDA REGISTRO EN BASE DE DATOS
+            registros_insert = cursor.rowcount
+            print(f'Registros insertados: {registros_insert}')
+
 except Exception as e:
     print(f'Error by {e}')
 finally:
     #ahora cerramos la conexion
     cursor.close()
-    conexion.close()
+    
