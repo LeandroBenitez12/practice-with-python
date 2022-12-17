6d6edc32a00484149dca441fe16bb954e4c793b7
Leandro Benitez <leansvae9@gmail.com>
2022-12-17 13:19:13 -0300

    Actualizar un registro

--- Universidad Python/Base de Datos/Leccion 01/prueba_db.py
+++ Universidad Python/Base de Datos/Leccion 01/prueba_db.py
@@ -6,17 +6,14 @@ try:
     with conexion:  #para asegurar que se cierre cualquier recuerso asociado a conecion se cierre
         #cursor es un obejto que nos permite ejecutar sentencias SQL en postgres
         with conexion.cursor() as cursor:
-            sentencia = 'INSERT INTO persona (name, surname, email) VALUES (%s, %s, %s)'
-            valores= (
-                ('Julio', 'ramones', 'bokec@type.com'),
-                ('Gabriel', 'romero', 'sahac@meil.com'),
-                ('Ezequiel', 'briasco', 'Jyn@hotmail.com'),
-            ) #tuplas de tupla valores
-
-            cursor.executemany(sentencia, valores)
+            sentencia = ' UPDATE persona SET name =%s, surname = %s, email = %s WHERE id_persona = %s'
+            primeros_valores = input('Ingrese las modificaciones (separada por coma):   ')
+            valores = tuple(primeros_valores.split(','))
+            #valores = ('Juan', 'Miguez', 'juansvae@gamil.com', 5)
+            cursor.execute(sentencia, valores)
             #conexion.commit()  GUARDA REGISTRO EN BASE DE DATOS
-            registros_insert = cursor.rowcount
-            print(f'Registros insertados: {registros_insert}')
+            registros_modified = cursor.rowcount
+            print(f'Registros modificados: {registros_modified}')
 
 except Exception as e:
     print(f'Error by {e}')
@@ -24,3 +21,14 @@ finally:
     #ahora cerramos la conexion
     cursor.close()
     
+
+
+
+#INSERT VARIOUS REGISTERS
+'''sentencia = 'INSERT INTO persona (name, surname, email) VALUES (%s, %s, %s)'
+            valores= (
+                ('Julio', 'ramones', 'bokec@type.com'),
+                ('Gabriel', 'romero', 'sahac@meil.com'),
+                ('Ezequiel', 'briasco', 'Jyn@hotmail.com'),
+            ) #tuplas de tupla valores
+'''
