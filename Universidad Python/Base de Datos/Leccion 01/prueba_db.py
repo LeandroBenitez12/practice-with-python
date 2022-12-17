58f956c02b61f61994160bdfcd9b44c1cbfa75f4
Leandro Benitez <leansvae9@gmail.com>
2022-12-17 12:08:35 -0300

    173. recuperando varios registros

--- Universidad Python/Base de Datos/Leccion 01/prueba_db.py
+++ Universidad Python/Base de Datos/Leccion 01/prueba_db.py
@@ -6,12 +6,20 @@ try:
     with conexion:  #para asegurar que se cierre cualquier recuerso asociado a conecion se cierre
         #cursor es un obejto que nos permite ejecutar sentencias SQL en postgres
         with conexion.cursor() as cursor:
-            #sentencia = 'SELECT * FROM persona WHERE id_persona = %s' #recuperar registros 
-            sentencia = 'SELECT * FROM persona WHERE id_persona IN (1,2)' #codigo duro
-            #id_persona = int(input('Ingrese el id_persona: '))
-            cursor.execute(sentencia, '''(id_persona,)''')#la ',' es para que sea una tupla y no una simple variable
+            #sentencia = 'SELECT * FROM persona WHERE id_persona = %s' #recuperar un registro 
+            sentencia = 'SELECT * FROM persona WHERE id_persona IN %s' #recuperar varios registros
+            
+            #id_persona = int(input('Ingrese el id_persona: ')) #para un registro
+            #llaves_primarias = ((1,2,3),)
+            entrada = input('Ingrese los id\'s a buscar(separados por comas):' )
+            llaves_primarias = (tuple(entrada.split(',')),)#EL SPLIT SACA LAS , y convertimos los numeros en tupla los ponemos en parentesis y hacemos otra tuplado de esa tupla 
+            
+            #cursor.execute(sentencia, '''(id_persona,)''') #la ',' es para que sea una tupla y no una simple variable
+            cursor.execute(sentencia, llaves_primarias)
+            
             registros = cursor.fetchall()   #regresa todos los registros
             #registro = cursor.fetchone()   #regresa un registro queda bien con lo de id_persona = %S ya que es nomas un registro
+            
             print(registros)
             #estamos conectados exitosamente a la base de datos
 except Exception as e:
