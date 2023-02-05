bd899c9ab3705862e908fce4c1762f60b4cd5bc1
Leandro Benitez <leansvae9@gmail.com>
2023-02-04 21:59:05 -0300

    profundizando python

---
+++ Universidad Python/profundizacion.py
@@ -0,0 +1,11 @@
+#Sistemas de numeracion
+
+#Convertir un str a int y luego ponerlo en los diferentes sistemas de numeracion
+
+#int
+#a = int('20', 10)   #no hace falta la base de diez ya que python lo tiene por default en sistema decimal
+#print(f'a = {a} decimal')
+
+#binario
+a = int('11010', 2)
+print(f'a = {a} decimal')
