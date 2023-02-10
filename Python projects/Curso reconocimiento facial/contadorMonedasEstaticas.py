baee022e69ea8cda8e73eeca1d177f0a4c65bf8b
Leandro Benitez <leansvae9@gmail.com>
2023-02-10 14:23:29 -0300

    Program Monedas encontradas

--- Python projects/Curso reconocimiento facial/contadorMonedasEstaticas.py
+++ Python projects/Curso reconocimiento facial/contadorMonedasEstaticas.py
@@ -1,15 +1,26 @@
 import cv2 as cv
 import numpy as np
 value_gauss= 3 
-original = cv.imread('D:\DESCARGAS\monedascontorno\monedascontorno\monedas.jpg')
+value_kernel=7
+original = cv.imread('C:\\Users\\juana\\OneDrive\\Documentos\\Downloads\\monedascontorno\\monedascontorno\\monedas.jpg')
 gray = cv.cvtColor(original , cv.COLOR_BGR2GRAY)
 gauss = cv.GaussianBlur(gray, (value_gauss , value_gauss), 0)
 canny = cv.Canny(gauss,50,99)
+kernel = np.ones((value_kernel, value_kernel), np.uint8)
+cierre= cv.morphologyEx(canny, cv.MORPH_CLOSE, kernel)
+
+
+contorno, jerarquia = cv.findContours(cierre.copy(), cv.RETR_EXTERNAL ,cv.CHAIN_APPROX_SIMPLE)
+
+print(f'Monedas encontradas: {format(len(contorno))}')
+cv.drawContours(original, contorno, -1, (255,50,130), 2)
+
 
 #mostrar resultado
 # cv.imshow('Original Image', original)
-cv.imshow('Gray Image', gray)
-cv.imshow('Gauss Image', gauss)
-cv.imshow('canny Image', canny)
+# cv.imshow('Gray Image', gray)
+# cv.imshow('Gauss Image', gauss)
+# cv.imshow('canny Image', canny)
+cv.imshow('Resultado', original)
 cv.waitKey(0)
 cv.destroyAllWindows()
