0a657e814edc1aae4887ce8fb7d46fb683e19b06
Leandro Benitez <leansvae9@gmail.com>
2023-02-15 17:10:44 -0300

    reconocimiento de monedas con exito , falla en la umbralizacion

--- Python projects/Curso reconocimiento facial/contadorMoneyCam.py
+++ Python projects/Curso reconocimiento facial/contadorMoneyCam.py
@@ -1,8 +1,11 @@
 import cv2 as cv
 import numpy as np
 
-def ordenarPutos(puntos):
-    n_puntos= np.concatenate(puntos[0],puntos[1],puntos[2],puntos[3]).tolist()
+un_peso_moneda_px = 9500
+diez_peso_moneda_px = 10500
+
+def ordenarPuntos(puntos):
+    n_puntos= np.concatenate([puntos[0],puntos[1],puntos[2],puntos[3]]).tolist()
     y_order = sorted(n_puntos, key=lambda n_puntos : n_puntos[1])
     x1_order = y_order[:2]
     x1_order = sorted(x1_order, key=lambda x1_order : x1_order[0])
@@ -14,16 +17,15 @@ def alineamiento(img,ancho,alto):
     imagen_alineada= None
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     type_umbral, umbral = cv.threshold(gray, 140, 255, cv.THRESH_BINARY)
-    cv.imshow('Umbral', umbral)
-    contorno, jerarquia = cv.findContours(umbral, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[0]
+    contorno= cv.findContours(umbral, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)[0]
     contorno = sorted(contorno, key= cv.contourArea,reverse=True)[:1]
     for c in contorno:
-        epsilon = .01*cv.arcLength(c,True)
+        epsilon = 0.01*cv.arcLength(c,True)
         approx = cv.approxPolyDP(c,epsilon,True)
-        if (approx)== 4:
-            puntos = ordenarPutos(approx)
+        if len(approx)== 4:
+            puntos = ordenarPuntos(approx)
             puntos1= np.float32(puntos)
-            puntos2= np.float32([[0,0],[ancho,0][0,alto],[ancho,alto]])
+            puntos2= np.float32([[0,0],[ancho,0],[0,alto],[ancho,alto]])
             Perspectiva = cv.getPerspectiveTransform(puntos1,puntos2)
             imagen_alineada = cv.warpPerspective(img, Perspectiva, (ancho,alto))
     return imagen_alineada
@@ -36,16 +38,16 @@ while True:
     if typeCam == False:
         break
 
-    imagen_A6 = alineamiento(enVivo, ancho= 480,alto=677)
+    imagen_A6 = alineamiento(enVivo, ancho= 480 ,alto=650)
 
     if imagen_A6 is not None:
         puntos= []
         gray = cv.cvtColor(imagen_A6,cv.COLOR_BGR2GRAY)
         blur = cv.GaussianBlur(gray,(5,5),1)
-        _,umbral = cv.threshold(blur, 0, 255, cv.THRESH_BINARY_INV)
-        cv.imshow('Umbral', umbral)
-        contorno2, jerarquia2 = cv.findContours(umbral, cv.RETR_EXTERNAL ,cv.CHAIN_APPROX_SIMPLE)[0]
-        cv.drawContours(imagen_A6, contorno2, -1, (0,255,0), 2 )
+        _,umbral2 = cv.threshold(blur, 0, 255, cv.THRESH_OTSU+cv.THRESH_BINARY_INV)
+        cv.imshow('Umbral2', umbral2)
+        contorno2= cv.findContours(umbral2, cv.RETR_EXTERNAL ,cv.CHAIN_APPROX_SIMPLE)[0]
+        cv.drawContours(imagen_A6, contorno2, -1, (0,0,255), 2 )
         suma1 = 0.0
         suma2 = 0.0
         for c2 in contorno2:
@@ -53,10 +55,23 @@ while True:
             momentos = cv.moments(c2)
             if (momentos["m00"]) ==0:
                 momentos["m00"]=1.0 #momento estatico
+            x= int( momentos["m10"]/ momentos["m00"])
+            y= int( momentos["m01"]/ momentos["m00"])
 
-
-    cv.imshow('En vivo', enVivo)
-    if ord() == 'q':
+            if area<10500 and area>9800:
+                font=cv.FONT_HERSHEY_SIMPLEX
+                cv.putText(imagen_A6, "10 pesos",(x,y) , font, 0.75, (0,255,0),2)
+                suma1=suma1+10.00
+            
+            if area<9500 and area>8500:
+                font=cv.FONT_HERSHEY_SIMPLEX
+                cv.putText(imagen_A6, "un peso",(x,y) , font, 0.75, (0,255,0),2)
+                suma2=suma2+1.0 
+        total = suma1 + suma2
+        print(f'Sumatoria total en pesos: ${round(total,2)}')
+        cv.imshow("Imagen A6", imagen_A6)
+        cv.imshow("enVivo" , enVivo)
+    if cv.waitKey(1)== ord('s'):
         break
 
 capturaVideo.release()
