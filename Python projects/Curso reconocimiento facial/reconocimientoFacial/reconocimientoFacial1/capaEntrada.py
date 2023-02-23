143c014506b34843c5ca516a4d411f2c34a45972
Leandro Benitez <leansvae9@gmail.com>
2023-02-22 23:56:38 -0300

    proyecto de caputras por reconocimiento facial y agregue material del curso

--- Python projects/Curso reconocimiento facial/reconocimientoFacial/reconocimientoFacial1/capaEntrada.py
+++ Python projects/Curso reconocimiento facial/reconocimientoFacial/reconocimientoFacial1/capaEntrada.py
@@ -1,32 +1,67 @@
 import cv2 as cv
 import numpy as np
-
+import os
+import imutils
+#declaramos rutas
+modelo= 'Caras Leandro'
+ruta1= 'C:\\Users\\juana\\Dropbox\\GIT\\practice-with-python\\Python projects\\Curso reconocimiento facial\\reconocimientoFacial\\reconocimientoFacial1'
+rutaCompleta =ruta1 +'\\'+ modelo
+#ruidos de archivo xml
 ruidos=cv.CascadeClassifier('C:\\Users\\juana\\Dropbox\\GIT\\practice-with-python\\Python projects\\Curso reconocimiento facial\\reconocimientoFacial\\Modelos de entrenamiento\\haarcascade_frontalface_default.xml')
+
+#creamos carpeta
+if not os.path.exists(rutaCompleta):
+    os.makedirs(rutaCompleta)
 
 #le decimos que la video captura es interna (0)
 camara = cv.VideoCapture(0)
 
+id = 0
 while True:
     #Encontramos la camara
-    type_cam, enVivo = camara.read()
-    
+    respuesta, enVivo = camara.read()
+    #bajamos resolucion para que no pese tanto al img
+    enVivo= imutils.resize(enVivo, width= 640)
+    if respuesta == False:
+        break
     #Pasamos el video a escala de grises
     gray= cv.cvtColor(enVivo, cv.COLOR_RGB2GRAY)
-    #
+
+    #detectamos las caras 
     caras =ruidos.detectMultiScale(gray,
     scaleFactor= 1.2,
     minNeighbors=3,
     minSize=(60,60),
     maxSize= (200,200))
-    font=cv.FONT_HERSHEY_SIMPLEX
-    for(x,y,h,b) in caras:
-        cv.rectangle(enVivo, (x,y), (x+h,y+b), (0,0,0), 2)
 
-    cv.putText(enVivo, "Mire Frontalmente a la camara y sonria", (50,400) , font, 0.75, (0,0,0),2)
-    cv.imshow('Cara', enVivo)
+    #fuente de la letra del video
+    font=cv.FONT_HERSHEY_DUPLEX 
+    #cuenta las capturas
     
-    if cv.waitKey(1) == ord('q'):
-        break
+    #ahorra recursos
+    idcaptura = enVivo.copy()
 
-camara.release
+    #Creamos nuestro rectangulo
+    for(x,y,h,b) in caras:
+        cv.rectangle(enVivo, (x,y), (x+b,y+h), (0,0,0), 2)
+        #
+        rostroCapturado=idcaptura[y:y+h,x:x+b]
+        rostroCapturado=cv.resize(rostroCapturado, [160,160], interpolation= cv.INTER_CUBIC)
+        cv.imwrite(rutaCompleta+f'\\img_{id}.jpg', rostroCapturado)
+        id = 1 +  id
+    
+    #mensaje en el video para el usuario
+    # cv.putText(enVivo, "Mire Frontalmente a la camara y sonria", (50,400) , font, 0.75, (0,0,0),2)
+    #mostramos camara en vivo con dettecion de rostros
+    cv.imshow('Cara', enVivo)
+    cv.waitKey(500)
+    #cerrar programa
+    if id == 11:
+        break
+    # if cv.waitKey(1) == ord('q'):
+    #     break
+
+#detener camara
+camara.release()
+#cerrar todas las ventanas
 cv.destroyAllWindows()
