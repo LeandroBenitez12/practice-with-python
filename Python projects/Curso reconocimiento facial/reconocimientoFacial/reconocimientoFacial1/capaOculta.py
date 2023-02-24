a4541a84067a9022e3de5e1c71b84569c82b00cc
Leandro Benitez <leansvae9@gmail.com>
2023-02-24 15:53:17 -0300

    creacion de sub carpeta y uso de method os.listdir()

---
+++ Python projects/Curso reconocimiento facial/reconocimientoFacial/reconocimientoFacial1/capaOculta.py
@@ -0,0 +1,7 @@
+import cv2 as cv
+import os
+import numpy as np
+
+dataRuta = 'C:\\Users\\juana\\Dropbox\\GIT\\practice-with-python\\Python projects\\Curso reconocimiento facial\\reconocimientoFacial\\reconocimientoFacial1\\dataFotos'
+listaData=os.listdir(dataRuta)
+print(listaData)
