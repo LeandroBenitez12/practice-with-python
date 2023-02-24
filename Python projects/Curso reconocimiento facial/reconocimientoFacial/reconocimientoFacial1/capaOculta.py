96b38fa514fe8c04abc1aded7e7491cac25d3e0b
Leandro Benitez <leansvae9@gmail.com>
2023-02-24 16:31:48 -0300

    proseguiremos

--- Python projects/Curso reconocimiento facial/reconocimientoFacial/reconocimientoFacial1/capaOculta.py
+++ Python projects/Curso reconocimiento facial/reconocimientoFacial/reconocimientoFacial1/capaOculta.py
@@ -4,4 +4,18 @@ import numpy as np
 
 dataRuta = 'C:\\Users\\juana\\Dropbox\\GIT\\practice-with-python\\Python projects\\Curso reconocimiento facial\\reconocimientoFacial\\reconocimientoFacial1\\dataFotos'
 listaData=os.listdir(dataRuta)
-print(listaData)
+# print(listaData)
+ids= []
+rostrosData= []
+id= 0
+
+for subCarpetas in listaData:
+    rutaCompleta= dataRuta + '\\' + subCarpetas 
+    for archivo in os.listdir(rutaCompleta):
+        ids.append(id)
+        rostrosData.append(cv.imread(rutaCompleta+ '\\'+ archivo,0))
+    id = id+1
+
+entrenamientoEigenFaceRecognizer= cv.face.EigenFaceRecognizer_create()
+print('Iniciando el entrenamiento, wait...')
+entrenamientoEigenFaceRecognizer.train(rostrosData, np.array(ids))
