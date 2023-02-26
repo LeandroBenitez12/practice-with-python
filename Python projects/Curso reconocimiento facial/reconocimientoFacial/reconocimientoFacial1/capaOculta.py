24dd00758dcd10862eaf5765b50194166b5c9599
Leandro Benitez <leansvae9@gmail.com>
2023-02-25 21:37:41 -0300

    Comente todo falta ela archivo .xml con el entrenamiento

--- Python projects/Curso reconocimiento facial/reconocimientoFacial/reconocimientoFacial1/capaOculta.py
+++ Python projects/Curso reconocimiento facial/reconocimientoFacial/reconocimientoFacial1/capaOculta.py
@@ -4,33 +4,44 @@ import numpy as np
 from time import time
 
 dataRuta = 'C:\\Users\\juana\\Dropbox\\GIT\\practice-with-python\\Python projects\\Curso reconocimiento facial\\reconocimientoFacial\\reconocimientoFacial1\\dataFotos'
-listaData=os.listdir(dataRuta)
+listaData = os.listdir(dataRuta)
 # print(listaData)
-ids= []
-rostrosData= []
-id= 0
+ids = []
+rostrosData = []
+id = 0
 
-TiempoInicial = time()  #iniciamos un tiempo con la funcion interna de python que retorna el time en segundos
-#bucle para acceder a las sub carpetas
+# iniciamos un tiempo con la funcion interna de python que retorna el time en segundos
+TiempoInicial = time()
+# bucle para acceder a las sub carpetas
 for subCarpetas in listaData:
-    rutaCompleta= dataRuta + '\\' + subCarpetas #entramos en las carpetas contenedoras de img 
+    # entramos en las carpetas contenedoras de img
+    rutaCompleta = dataRuta + '\\' + subCarpetas
 
-    print('Iniciando lecturas... ') #damos inicio a las lecturas de archivos
-    for archivo in os.listdir(rutaCompleta): #bucle para acceder a los archivos de las sub carpetas
-        print('Imagenes: ',subCarpetas + '/' + archivo) #imprimimos cada img que accedemos
-        ids.append(id)  #diferenciar las imagenes de cada carpeta contenedora de imagenes
-        rostrosData.append(cv.imread(rutaCompleta+ '\\'+ archivo,0))    #pasar las img a escala de grises(sobreescribiendolo) y acceder a cada archivo
+    print('Iniciando lecturas... ')  # damos inicio a las lecturas de archivos
+    # bucle para acceder a los archivos de las sub carpetas
+    for archivo in os.listdir(rutaCompleta):
+        # imprimimos cada img que accedemos
+        print('Imagenes: ', subCarpetas + '/' + archivo)
+        # diferenciar las imagenes de cada carpeta contenedora de imagenes
+        ids.append(id)
+        # pasar las img a escala de grises(sobreescribiendolo) y acceder a cada archivo
+        rostrosData.append(cv.imread(rutaCompleta + '\\' + archivo, 0))
 
     id = id+1   # para darle diferencias a cada carpeta contenedora de imagenes
-    tiempoFinalLectura = time() # tiempo final donde se finalizaron las lecturas
-    tiempoTotalLectura = tiempoFinalLectura - TiempoInicial #calculamos el tiempo total de lectura restando el tiempo final - el inicial
-    print('Tiempo total lectura: ',tiempoTotalLectura)  #imprimimos el tiempo total de lectura
+    tiempoFinalLectura = time()  # tiempo final donde se finalizaron las lecturas
+    # calculamos el tiempo total de lectura restando el tiempo final - el inicial
+    tiempoTotalLectura = tiempoFinalLectura - TiempoInicial
+    # imprimimos el tiempo total de lectura
+    print('Tiempo total lectura: ', tiempoTotalLectura)
 
-entrenamientoEigenFaceRecognizer= cv.face.EigenFaceRecognizer_create()
-print('Iniciando el entrenamiento, wait...')    #iniciamos el entrenamiento
-entrenamientoEigenFaceRecognizer.train(rostrosData, np.array(ids))  
-tiempoFinalEntrenamiento = time()   #tiempo final en segundos
-tiempoTotalEntrenamiento = tiempoFinalEntrenamiento - tiempoTotalLectura #hacemos el calculo de tiempo que llevo el traning 
-print('Tiempo total Entrenamiento: ',tiempoTotalEntrenamiento)  # imprimimos el tiempo total del training
-entrenamientoEigenFaceRecognizer.write('EntrenamientoEigenFaceRecognizer.xml') # escribimos como se va a nombrar el archivo creado
-print('Entrenamiento completado...') # finalizamos training
+entrenamientoEigenFaceRecognizer = cv.face.EigenFaceRecognizer_create()
+print('Iniciando el entrenamiento, wait...')  # iniciamos el entrenamiento
+entrenamientoEigenFaceRecognizer.train(rostrosData, np.array(ids))
+tiempoFinalEntrenamiento = time()  # tiempo final en segundos
+# hacemos el calculo de tiempo que llevo el traning
+tiempoTotalEntrenamiento = tiempoFinalEntrenamiento - tiempoTotalLectura
+# imprimimos el tiempo total del training
+print('Tiempo total Entrenamiento: ', tiempoTotalEntrenamiento)
+# escribimos como se va a nombrar el archivo creado
+entrenamientoEigenFaceRecognizer.write('EntrenamientoEigenFaceRecognizer.xml')
+print('Entrenamiento completado...')  # finalizamos training
