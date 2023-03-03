e666fe2d09daddd8cbd8daf58fea2a26b8a1c5e4
Leandro Benitez <leansvae9@gmail.com>
2023-03-03 13:41:56 -0300

    creamos un test de capturas para configurar mejor las capturas de rostros

--- Python projects/Curso reconocimiento facial/reconocimientoFacial/reconocimientoFacial1/testCapture/testCapture.py
+++ Python projects/Curso reconocimiento facial/reconocimientoFacial/reconocimientoFacial1/testCapture/testCapture.py
@@ -1,20 +1,9 @@
 import cv2 as cv
-import numpy as np
-import os
-import imutils
 
-#declaramos rutas
 
-#PONEMOS NOMBRE A LA CARPETA
-modelo= 'Auron Play'
-ruta1= 'C:/Users/juana/Dropbox/GIT/practice-with-python/Python projects/Curso reconocimiento facial/reconocimientoFacial/reconocimientoFacial1/dataFotos'
-rutaCompleta =ruta1 +'/'+ modelo
 #ruidos de archivo xml
 ruidos=cv.CascadeClassifier('C:/Users/juana/Dropbox/GIT/practice-with-python/Python projects/Curso reconocimiento facial/reconocimientoFacial/Modelos de entrenamiento/haarcascade_frontalface_default.xml')
 
-#creamos carpeta
-if not os.path.exists(rutaCompleta):
-    os.makedirs(rutaCompleta)
 
 #En vivo le decimos que la video captura es interna (0)
 camara = cv.VideoCapture(0)
@@ -22,20 +11,17 @@ camara = cv.VideoCapture(0)
 # camara = cv.VideoCapture('C:/Users/juana/Dropbox/GIT/practice-with-python/Python projects/Curso reconocimiento facial/reconocimientoFacial/reconocimientoFacial1/videos/videoauron.mp4')
 #video Elon
 # camara = cv.VideoCapture('C:/Users/juana/Dropbox/GIT/practice-with-python/Python projects/Curso reconocimiento facial/reconocimientoFacial/reconocimientoFacial1/videos/ElonMusk.mp4')
-#cuenta las capturas
-id = 0
 while True:
     #Encontramos la camara
     respuesta, enVivo = camara.read()
     #bajamos resolucion para que no pese tanto al img
-    enVivo= imutils.resize(enVivo, width= 640)
     if respuesta == False:
         break
     #Pasamos el video a escala de grises
     gray= cv.cvtColor(enVivo, cv.COLOR_RGB2GRAY)
 
     #detectamos las caras
-    caras =ruidos.detectMultiScale(gray,1.25,3)
+    caras =ruidos.detectMultiScale(gray,1.25,5)
 
     #fuente de la letra del video
     font=cv.FONT_HERSHEY_DUPLEX
@@ -46,22 +32,16 @@ while True:
     #Creamos nuestro rectangulo
     for(x,y,h,b) in caras:
         cv.rectangle(enVivo, (x,y), (x+b,y+h), (0,0,0), 2)
-        #
-        rostroCapturado=idcaptura[y:y+h,x:x+b]
-        rostroCapturado=cv.resize(rostroCapturado, [600,600], interpolation= cv.INTER_CUBIC)
-        cv.imwrite(rutaCompleta+f'/img_{id}.jpg', rostroCapturado)
-        id = 1 +  id
 
     #mensaje en el video para el usuario
-    # cv.putText(enVivo, "Mire Frontalmente a la camara y sonria", (50,400) , font, 0.75, (0,0,0),2)
+    cv.putText(enVivo, "Mire Frontalmente a la camara y sonria", (50,400) , font, 0.75, (0,0,0),2)
     #mostramos camara en vivo con dettecion de rostros
     cv.imshow('Resultado', enVivo)
     # cv.waitKey(200)
     #cerrar programa
-    if id == 51:
+    if cv.waitKey(1) == ord('s'):
         break
-    # if cv.waitKey(1) == ord('q'):
-    #     break
+
 
 #detener camara
 camara.release()
