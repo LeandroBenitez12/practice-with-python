8e705d522ecad8bca4eeda98609d2addca8dd28f
Leandro Benitez <108737751+LeandroBenitez12@users.noreply.github.com>
2023-03-17 18:05:12 -0300

    mejoras de identacion y a la hora de imprimir resultados

--- Games/piedra papel o tijera/game.py
+++ Games/piedra papel o tijera/game.py
@@ -2,42 +2,64 @@
 PIEDRA = 0
 PAPEL = 1
 TIJERA = 2
+MIN_VALOR = 0
+MAX_VALOR = 3
 repeticiones = int(input('Introduzca el numero de repeticiones(Impar): '))
 resultado= 'Empezemos el juego...'
 print(resultado)
 id = 0
-count_win_1 = 0
-count_win_2 = 0
+rondas_ganadas_jugador_1 = 0
+rondas_ganadas_jugador_2 = 0
+
 while id < repeticiones:
     dato_jugador_1 = int(input("Ingrese valor para jugador Nº1 (Piedra[1]/Papel[2]/Tijera[3]): "))
     dato_jugador_2 = int(input("Ingrese valor para jugador Nº2 (Piedra[1]/Papel[2]/Tijera[3]): "))
 
-    if dato_jugador_1 and dato_jugador_2 > 0 and dato_jugador_1 and dato_jugador_2  <= 3:
+    if dato_jugador_1 and dato_jugador_2 > MIN_VALOR and dato_jugador_1 and dato_jugador_2  <= MAX_VALOR:
         if dato_jugador_1 == dato_jugador_2:
             resultado = 'Empate'
             print(resultado)
         elif dato_jugador_1 == PIEDRA and dato_jugador_2 == PAPEL:
-            count_win_2 += 1
+            resultado = 'Ganador de ronda: Jugador Nº2'
+            rondas_ganadas_jugador_2 += 1
+            print(resultado + f', Rondas ganadas acumuludas : {rondas_ganadas_jugador_2}')
+
         elif dato_jugador_1 == PIEDRA and dato_jugador_2 == TIJERA:
-            count_win_1 += 1
+            resultado = 'Ganador de ronda: Jugador Nº1 '
+            rondas_ganadas_jugador_1 += 1
+            print(resultado + f', Rondas ganadas acumuludas : {rondas_ganadas_jugador_1}')
+
         elif dato_jugador_1 == PAPEL and dato_jugador_2 == PIEDRA:
-            count_win_1 += 1
+            resultado = 'Ganador de ronda: Jugador Nº1 '
+            rondas_ganadas_jugador_1 += 1
+            print(resultado + f', Rondas ganadas acumuludas : {rondas_ganadas_jugador_1}')
+
         elif dato_jugador_1 == PAPEL and dato_jugador_2 == TIJERA:
-            count_win_2 += 1
+            resultado = 'Ganador de ronda: Jugador Nº2 '
+            rondas_ganadas_jugador_2 += 1
+            print(resultado + f', Rondas ganadas acumuludas : {rondas_ganadas_jugador_2}')
+
         elif dato_jugador_1 == TIJERA and dato_jugador_2 == PIEDRA:
-            count_win_2 += 1
+            resultado = 'Ganador de ronda: Jugador Nº2 '
+            rondas_ganadas_jugador_2 += 1
+            print(resultado + f', Rondas ganadas acumuludas : {rondas_ganadas_jugador_2}')
+
         elif dato_jugador_1 == TIJERA and dato_jugador_2 == PAPEL:
-            count_win_1 += 1
+            resultado = 'Ganador de ronda: Jugador Nº1 '
+            rondas_ganadas_jugador_1 += 1
+            print(resultado + f', Rondas ganadas acumuludas : {rondas_ganadas_jugador_1}')
+
         
     id += 1
     if id == repeticiones :
-        if count_win_1 < count_win_2:
-            resultado = 'Winner jugador 2'
-        if count_win_1 > count_win_2:
-            resultado = 'Winner jugador 1' 
-
-
-print(f' The result is : {resultado}')
+        if rondas_ganadas_jugador_1 < rondas_ganadas_jugador_2:
+            resultado = 'Ganador jugador 2'
+        if rondas_ganadas_jugador_1 > rondas_ganadas_jugador_2:
+            resultado = 'Ganador jugador 1' 
+        if rondas_ganadas_jugador_1 == rondas_ganadas_jugador_2:
+            resultado = 'Partida empatada' 
+print('')
+print(f'El resultado es : {resultado}')
 
             
 
