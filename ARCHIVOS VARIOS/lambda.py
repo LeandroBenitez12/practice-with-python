8d33daefe703d44e22cf4f5fd7ba69c696ccaa90
Leandro Benitez <108737751+LeandroBenitez12@users.noreply.github.com>
2023-03-17 20:36:55 -0300

    pruebas con lambda

---
+++ ARCHIVOS VARIOS/lambda.py
@@ -0,0 +1,9 @@
+
+# A lambda function is a small anonymous function.
+# A lambda function can take any number of arguments, but can only have one expression.
+a = 5
+c= 1
+
+b = lambda x: x + a * c
+
+print(b(15))
