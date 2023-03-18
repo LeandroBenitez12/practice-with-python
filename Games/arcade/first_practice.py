48bd569777c7fcc8d5a5ea325835bbf20d1e740e
Leandro Benitez <108737751+LeandroBenitez12@users.noreply.github.com>
2023-03-18 16:20:47 -0300

    mejora de declaracion de variables

--- Games/arcade/first_practice.py
+++ Games/arcade/first_practice.py
@@ -9,6 +9,14 @@ SCREEN_TITULO  = 'PRUEBA 1'
 
 
 
+# funciones 
+def getGrilla(min_dist_borde, max_dist_borde,separacionGrillas):
+    for x in range(min_dist_borde, max_dist_borde, separacionGrillas):
+        arc.draw_line(start_x=x , start_y= min_dist_borde, end_x = x, end_y= max_dist_borde, color= arc.color.BLUE_BELL, line_width= 3)
+
+    for y in range( min_dist_borde, max_dist_borde, separacionGrillas):
+        arc.draw_line(start_x=min_dist_borde,start_y=y, end_x=max_dist_borde, end_y=y, color=arc.color.BLUE_BELL, line_width= 3)
+
 # abrir ventana
 arc.open_window(SCREEN_ANCHO, SCREEN_LARGO, SCREEN_TITULO)
 
@@ -17,13 +25,8 @@ arc.set_background_color(arc.color.WHITE_SMOKE)
 
 #limpia la screen y dibuja
 arc.start_render()
-
 # dibujo matriz de 3 x 3 para poner las figuras
-for x in range(100, 701, 200):
-    arc.draw_line(start_x=x , start_y= 100, end_x = x, end_y= 700, color= arc.color.BLUE_BELL, line_width= 3)
-
-for y in range( 100, 701, 200):
-    arc.draw_line(start_x=100,start_y=y, end_x=700, end_y=y, color=arc.color.BLUE_BELL, line_width= 3)
+getGrilla(min_dist_borde=100, max_dist_borde= 701, separacionGrillas= 200)
 
 # deja de dibujar
 arc.finish_render()
