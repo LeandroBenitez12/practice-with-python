4048b6026a789eca57606fc8466119ea8d38461a
Leandro Benitez <108737751+LeandroBenitez12@users.noreply.github.com>
2023-03-17 22:03:08 -0300

    identado y resolucion de numero par

--- Games/piedra papel o tijera/game.py
+++ Games/piedra papel o tijera/game.py
@@ -5,71 +5,82 @@ TIJERA = 3
 
 MIN_VALOR = 0
 MAX_VALOR = 3
-repeticiones = int(input('Introduzca el numero de repeticiones(Impar): '))
-
-for i in range(2, repeticiones):
-    repeticiones = repeticiones % 2
-resultado= 'Empezemos el juego...'
-print(resultado)
 id = 0
 rondas_ganadas_jugador_1 = 0
 rondas_ganadas_jugador_2 = 0
+resultado = 'Empezemos el juego...'
+repeticiones = int(input('Introduzca el numero de repeticiones(Impar): '))
 
-while id < repeticiones:
-    dato_jugador_1 = int(input("Ingrese valor para jugador Nº1 (Piedra[1]/Papel[2]/Tijera[3]): "))
-    dato_jugador_2 = int(input("Ingrese valor para jugador Nº2 (Piedra[1]/Papel[2]/Tijera[3]): "))
 
-    if dato_jugador_1 and dato_jugador_2 > MIN_VALOR and dato_jugador_1 and dato_jugador_2  <= MAX_VALOR:
-        if dato_jugador_1 == dato_jugador_2:
-            resultado = 'Empate'
-            print(resultado)
-        elif dato_jugador_1 == PIEDRA and dato_jugador_2 == PAPEL:
-            resultado = 'Ganador de ronda: Jugador Nº2'
-            rondas_ganadas_jugador_2 += 1
-            print(resultado + f', Rondas ganadas acumuludas : {rondas_ganadas_jugador_2}')
-
-        elif dato_jugador_1 == PIEDRA and dato_jugador_2 == TIJERA:
-            resultado = 'Ganador de ronda: Jugador Nº1 '
-            rondas_ganadas_jugador_1 += 1
-            print(resultado + f', Rondas ganadas acumuludas : {rondas_ganadas_jugador_1}')
-
-        elif dato_jugador_1 == PAPEL and dato_jugador_2 == PIEDRA:
-            resultado = 'Ganador de ronda: Jugador Nº1 '
-            rondas_ganadas_jugador_1 += 1
-            print(resultado + f', Rondas ganadas acumuludas : {rondas_ganadas_jugador_1}')
-
-        elif dato_jugador_1 == PAPEL and dato_jugador_2 == TIJERA:
-            resultado = 'Ganador de ronda: Jugador Nº2 '
-            rondas_ganadas_jugador_2 += 1
-            print(resultado + f', Rondas ganadas acumuludas : {rondas_ganadas_jugador_2}')
-
-        elif dato_jugador_1 == TIJERA and dato_jugador_2 == PIEDRA:
-            resultado = 'Ganador de ronda: Jugador Nº2 '
-            rondas_ganadas_jugador_2 += 1
-            print(resultado + f', Rondas ganadas acumuludas : {rondas_ganadas_jugador_2}')
-
-        elif dato_jugador_1 == TIJERA and dato_jugador_2 == PAPEL:
-            resultado = 'Ganador de ronda: Jugador Nº1 '
-            rondas_ganadas_jugador_1 += 1
-            print(resultado + f', Rondas ganadas acumuludas : {rondas_ganadas_jugador_1}')
-        
-        id += 1
+def esImpar():
+    dato = repeticiones % 2
+    if dato == 1:
+        return True
     else:
-        print('Error , no existe la opcion para el numero ingresado')
-        
-    
-    if id == repeticiones :
-        if rondas_ganadas_jugador_1 < rondas_ganadas_jugador_2:
-            resultado = 'Ganador jugador 2'
-        if rondas_ganadas_jugador_1 > rondas_ganadas_jugador_2:
-            resultado = 'Ganador jugador 1' 
-        if rondas_ganadas_jugador_1 == rondas_ganadas_jugador_2:
-            resultado = 'Partida empatada' 
+        return False
+
+
+if esImpar():
+    print(resultado)
+    while id < repeticiones:
+        dato_jugador_1 = int(
+            input("Ingrese valor para jugador Nº1 (Piedra[1]/Papel[2]/Tijera[3]): "))
+        dato_jugador_2 = int(
+            input("Ingrese valor para jugador Nº2 (Piedra[1]/Papel[2]/Tijera[3]): "))
+
+        if dato_jugador_1 and dato_jugador_2 > MIN_VALOR and dato_jugador_1 and dato_jugador_2 <= MAX_VALOR:
+            if dato_jugador_1 == dato_jugador_2:
+                resultado = 'Empate'
+                print(resultado)
+            elif dato_jugador_1 == PIEDRA and dato_jugador_2 == PAPEL:
+                resultado = 'Ganador de ronda: Jugador Nº2'
+                rondas_ganadas_jugador_2 += 1
+                print(
+                    resultado + f', Rondas ganadas acumuludas : {rondas_ganadas_jugador_2}')
+
+            elif dato_jugador_1 == PIEDRA and dato_jugador_2 == TIJERA:
+                resultado = 'Ganador de ronda: Jugador Nº1 '
+                rondas_ganadas_jugador_1 += 1
+                print(
+                    resultado + f', Rondas ganadas acumuludas : {rondas_ganadas_jugador_1}')
+
+            elif dato_jugador_1 == PAPEL and dato_jugador_2 == PIEDRA:
+                resultado = 'Ganador de ronda: Jugador Nº1 '
+                rondas_ganadas_jugador_1 += 1
+                print(
+                    resultado + f', Rondas ganadas acumuludas : {rondas_ganadas_jugador_1}')
+
+            elif dato_jugador_1 == PAPEL and dato_jugador_2 == TIJERA:
+                resultado = 'Ganador de ronda: Jugador Nº2 '
+                rondas_ganadas_jugador_2 += 1
+                print(
+                    resultado + f', Rondas ganadas acumuludas : {rondas_ganadas_jugador_2}')
+
+            elif dato_jugador_1 == TIJERA and dato_jugador_2 == PIEDRA:
+                resultado = 'Ganador de ronda: Jugador Nº2 '
+                rondas_ganadas_jugador_2 += 1
+                print(
+                    resultado + f', Rondas ganadas acumuludas : {rondas_ganadas_jugador_2}')
+
+            elif dato_jugador_1 == TIJERA and dato_jugador_2 == PAPEL:
+                resultado = 'Ganador de ronda: Jugador Nº1 '
+                rondas_ganadas_jugador_1 += 1
+                print(
+                    resultado + f', Rondas ganadas acumuludas : {rondas_ganadas_jugador_1}')
+
+            id += 1
+        else:
+            print('Error , no existe la opcion para el numero ingresado')
+
+        if id == repeticiones:
+            if rondas_ganadas_jugador_1 < rondas_ganadas_jugador_2:
+                resultado = 'Ganador jugador 2'
+            if rondas_ganadas_jugador_1 > rondas_ganadas_jugador_2:
+                resultado = 'Ganador jugador 1'
+            if rondas_ganadas_jugador_1 == rondas_ganadas_jugador_2:
+                resultado = 'Partida empatada'
+else:
+    print(
+        f'el numero {repeticiones} no es impar , introduzca un numero impar porfavor')
 print('')
 print(f'El resultado es : {resultado}')
-
-            
-
-
-
-
