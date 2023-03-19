d7e50fc8863238b74f09e3008f0362c7c1c4dfda
Leandro Benitez <108737751+LeandroBenitez12@users.noreply.github.com>
2023-03-18 22:08:29 -0300

    function getpoint in matriz 1-1

--- Games/arcade/first_practice.py
+++ Games/arcade/first_practice.py
@@ -10,16 +10,20 @@ SCREEN_TITULO  = 'PRUEBA 1'
 
 
 # funciones 
-def getGrilla(min_dist_borde, max_dist_borde,separacionGrillas):
-    for x in range(min_dist_borde, max_dist_borde, separacionGrillas):
+def getMatriz(min_dist_borde, max_dist_borde,separacionMatrices):
+    for x in range(min_dist_borde, max_dist_borde, separacionMatrices):
         arc.draw_line(start_x=x , start_y= min_dist_borde, end_x = x, end_y= max_dist_borde, color= arc.color.BLUE_BELL, line_width= 3)
 
-    for y in range( min_dist_borde, max_dist_borde, separacionGrillas):
+    for y in range( min_dist_borde, max_dist_borde, separacionMatrices):
         arc.draw_line(start_x=min_dist_borde,start_y=y, end_x=max_dist_borde, end_y=y, color=arc.color.BLUE_BELL, line_width= 3)
 
-def getpoint(x,y):
-    arc.draw_text('Draw point', x, y, arc.color.BLACK)
-    arc.draw_point()
+def getPoint():
+    x_point = 200
+    y_point = 600
+    x_text = 108
+    y_text =  504
+    arc.draw_text('Draw point', x_text, y_text, arc.color.BLACK)
+    arc.draw_point(x_point, y_point, arc.color.GREEN, 10)
 
 # abrir ventana
 arc.open_window(SCREEN_ANCHO, SCREEN_LARGO, SCREEN_TITULO)
@@ -30,8 +34,10 @@ arc.set_background_color(arc.color.WHITE_SMOKE)
 #limpia la screen y dibuja
 arc.start_render()
 # dibujo matriz de 3 x 3 para poner las figuras
-getGrilla(min_dist_borde=100, max_dist_borde= 701, separacionGrillas= 200)
+getMatriz(min_dist_borde=100, max_dist_borde= 701, separacionMatrices= 200)
 
+# dijuo en la primera fila primera columna un 
+getPoint()
 # deja de dibujar
 arc.finish_render()
 
