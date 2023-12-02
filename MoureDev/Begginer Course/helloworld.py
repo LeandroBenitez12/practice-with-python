914793e1fef2e5f5f77ea66b08e3a1a6e1476ed2
Leandro Benitez <108737751+LeandroBenitez12@users.noreply.github.com>
2023-12-02 15:39:37 -0300

    lists, sec, dic, tuple

--- MoureDev/Begginer Course/helloworld.py
+++ MoureDev/Begginer Course/helloworld.py
@@ -24,16 +24,16 @@ result = my_int + my_float + my_double # sum of different variables
 # result = my_int + my_float + my_string_full_name # error
 print_variable(result)
 
-my_bool = True
+my_bool = False
 
 # transform data types 
 print("hola soy leandro y tengo "+ str(my_int) + " y mido " + str(my_float))
 print(f"hola soy leandro y tengo {my_int} y mido {my_bool}")
 
 # lists
-my_list = [my_string_full_name, my_int, my_bool, my_float, my_double]
+my_list = [my_string_full_name, my_int, my_bool, my_float, my_double] # podes agregar nuevos/remover valores, es ordenada
 my_edad = 12
-my_list.append(my_edad)
+my_list.append(my_edad) # added new value to the list
 print_variable(my_list)
 print_variable(my_list[4])
 
@@ -42,3 +42,10 @@ my_dic = { "string" : my_string_full_name, "edad" : my_int, "bool" : my_bool, "p
 
 print_variable(my_dic)
 print_variable(my_dic["edad"])
+
+#set , permite no duplicar los valores y te entrega la info de forma desordenada
+my_set = {my_string_full_name, my_int, my_bool, False,  my_float, my_double, my_double, my_string_full_name, my_edad, my_string_full_name}
+print_variable(my_set)
+
+# tuple , una forma de almacenar valores que no puedan cambiar, esten ordenados y se puedan repetir
+my_tuple ={my_string_full_name, my_int, my_bool, False,  my_float, my_double, my_double, my_string_full_name, my_edad, my_string_full_name}
