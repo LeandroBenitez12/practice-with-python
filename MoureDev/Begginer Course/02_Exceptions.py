ff139a448789b3e3fdeebbea551924042837521b
Leandro Benitez <108737751+LeandroBenitez12@users.noreply.github.com>
2023-12-06 22:53:58 -0300

    try except

--- MoureDev/Begginer Course/02_Exceptions.py
+++ MoureDev/Begginer Course/02_Exceptions.py
@@ -1,1 +1,9 @@
 # Exception Handling ( Manejo de errores )
+
+try:
+    number_one = int(input("Ingrese un numero: "))
+    numbre_two = int(input("Ingrese un segundo numero: "))
+    print(f" La suma del numero {number_one} + {numbre_two} es igual a {number_one + numbre_two} ")
+    print("No ocurrio error")
+except:
+    print("Ha ocurrido un error")
